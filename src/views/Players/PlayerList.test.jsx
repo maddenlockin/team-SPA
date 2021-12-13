eb9d44b4f174ee20347fe85dff28eecc688b158e
maddenlockin <maddenlockin@gmail.com>
2021-12-12 19:45:54 -0800

    player list test updated and passing

--- src/views/Players/PlayerList.test.jsx
+++ src/views/Players/PlayerList.test.jsx
@@ -1,18 +1,57 @@
 import { render, screen } from "@testing-library/react";
+import { setupServer } from "msw/node";
+import { rest } from "msw";
 import { MemoryRouter } from "react-router"
+import userEvent from "@testing-library/user-event";
 import PlayerList from "./PlayerLIst";
 
+const mockPlayer1 = {
+    id: 1,
+    created_at: '',
+    name: 'Name Too',
+    position: 'Outfield',
+    team_id: 1
+};
+
+const mockPlayer2 = {
+    id: 2,
+    created_at: '',
+    name: 'Name You',
+    position: 'Poutfield',
+    team_id: 1
+}
+
+const mock = [mockPlayer1, mockPlayer2]
+
+const server = setupServer(
+    rest.get('url', (req, res, ctx) => {
+        return res(
+            ctx.json(mock)
+        );
+    }),
+    rest.delete('url', (req, res, ctx) => {
+        return res(ctx.json(mockPlayer2));
+    })
+);
+
+beforeAll(() => {
+    server.listen();
+});
+
+afterAll(() => {
+    server.close();
+});
+
 it('should render a list of players', async () => {
-    render(
+    const {container} = render(
         <MemoryRouter>
             <PlayerList />
         </MemoryRouter>
     );
 
-    const player1 = await screen.findByText('Kary Chair');
-    const player2 = await screen.findByText('Lampy Vampy');
-
-
-    expect(player1).toBeInTheDocument();
-    expect(player2).toBeInTheDocument();
+    const player = await screen.findByText(/Boppy Boop/);
+    const playerTwo = await screen.findByText(/Finney The Elder Cat/);
+    expect(player).toBeInTheDocument();
+    expect(playerTwo).toBeInTheDocument();
+    expect(container).toMatchSnapshot();
 })
